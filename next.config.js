ea674f4104b7b24614594a27537bdddd9dfd2e9b
ali <ali.sbiaazayen@outlook.com>
2024-01-20 16:35:15 +0100

    prefix of the url

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const repo = 'Analytics-Web'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
+
 const nextConfig = {
     images: {
         loader: 'akamai',
         path: '',
     },
-    assetPrefix: './',
+    assetPrefix,
+    basePath,
 };
+
 
 module.exports = nextConfig;
